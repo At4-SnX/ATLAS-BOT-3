20a9f326b77e910004e37fe333a40d73bd742ffa
Adam Raynaut <snx123123@gmail.com>
2026-07-24 15:33:14 +0200

    zsdq

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ const CATEGORY_LABELS_FR = {
 
 module.exports = {
   TOKEN: readVar("DISCORD_TOKEN", { required: true }),
-  THEME_COLOR: readVar("THEME_COLOR", { fallback: "#00eeff" }),
+  THEME_COLOR = Number(readVar("THEME_COLOR", { fallback: "#00eeff" }).trim().replace("#", "0x"));
   STAFF_ROLE,
   LOG_CHANNEL: readVar("LOG_CHANNEL_ID", { required: true }),
   PING_ROLES,
